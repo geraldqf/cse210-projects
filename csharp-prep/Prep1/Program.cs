fa1a408275dfe531db8df6b6e9c6a96dbdcbda7f
geraldqf <qui17007@byui.edu>
2023-01-04 11:56:53 -0500

    Comments added

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask for your first name
         Console.WriteLine("What is your first name? ");
         String first_name = Console.ReadLine();
+        // Ask for your last name
         Console.WriteLine("What is your last name? ");
         String last_name = Console.ReadLine();
+
+        // Show the greeting like James Bond style
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }
