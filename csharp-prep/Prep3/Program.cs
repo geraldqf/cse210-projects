5511250051707308a922126634cb02bd9a84b6af
geraldqf <qui17007@byui.edu>
2023-01-07 16:42:52 -0500

    Prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // String magicNumber = Console.ReadLine();
+        // int magicNumberInt = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1, 100);
+
+        String guessNumber;
+        int guessNumberInt;
+        
+        do
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = Console.ReadLine();
+            guessNumberInt = int.Parse(guessNumber);
+
+            if (guessNumberInt > magicNumberInt)
+            {
+                Console.WriteLine("Lower");  
+            }
+            else if (guessNumberInt < magicNumberInt)
+            {
+                Console.WriteLine("Higher"); 
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            } 
+
+        } while (guessNumberInt != magicNumberInt);
     }
 }
