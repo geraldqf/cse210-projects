3f091dec948aad35e3705a93c07ed15ce218f48e
geraldqf <qui17007@byui.edu>
2023-01-12 11:27:07 -0500

    Week03 Abstraction Learning Activity Done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Console.WriteLine($"{job1._company}");
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Console.WriteLine($"{job2._company}");
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allisson Rose";
+        myResume._jobList.Add(job1);
+        myResume._jobList.Add(job2);
+
+        // Console.WriteLine($"{myResume._jobList[0]._jobTitle}");
+
+        myResume.DisplayResume();
+
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+public class Resume
+{
+    public string _name = "";
+
+    public List<Job> _jobList = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"{_name}");
+
+        foreach (Job job in _jobList)
+        {
+            job.DisplayJobDetails();
+        }
+
+    }
+
+}
