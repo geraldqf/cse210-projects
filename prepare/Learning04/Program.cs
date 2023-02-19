8bcd47aecdf868996ab45f9fc5c75b9e82b4d809
geraldqf <qui17007@byui.edu>
2023-02-19 15:37:52 -0500

    All the requirements done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication"); 
+        Console.WriteLine(myAssignment.GetSummary());
+
+        MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguez","Fractions","7.3","8-19");
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+        Console.WriteLine(myMathAssignment.GetSummary());
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World Was II");
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
+    
 }
